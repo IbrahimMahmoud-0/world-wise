24a639f79c64b45c50f5cfd5076c8b07d735992a
IbrahimMahmoud-0 <0ibrahimmahmoud@gmail.com>
2024-03-22 14:00:34 +0200

    stage 1

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
+import { CitiesProvider } from "./contexts/CitiesContext.jsx";
+// const BASE_URL = "http://localhost:9000";
+
+function App() {
+  return (
+    <div>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </div>
+  );
+}
+
+export default App;
