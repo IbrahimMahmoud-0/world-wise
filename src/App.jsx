839417287b0caa1b01551799f193ff99ab87562b
IbrahimMahmoud-0 <0ibrahimmahmoud@gmail.com>
2024-03-25 14:12:00 +0200

    Fake Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ import CountryList from "./components/CountryList";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
 import { CitiesProvider } from "./contexts/CitiesContext.jsx";
+import { AuthProvider } from "./contexts/fakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 // const BASE_URL = "http://localhost:9000";
 
 function App() {
   return (
-    <div>
+    <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
@@ -23,7 +25,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate to="cities" replace />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
@@ -34,7 +43,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </CitiesProvider>
-    </div>
+    </AuthProvider>
   );
 }
 
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map.jsx";
+import User from "../components/User.jsx";
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
